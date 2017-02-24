3d1b3fea2959e94dc84bf8f1845e0071de3c8cae
yanggb <muyuqiu001@163.com>
2017-02-24 22:58:54 +0800

    #1 将内部写死的数据简单的使用表的结构展示出来 https://github.com/muyuqiu001/displayData/issues/1#issuecomment-282301836

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,9 +5,54 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
+        <div>
+            <table border="2">
+                <thead>
+                    <td>
+                        Commit Id
+                    </td>
+                    <td>
+                         Reivew Person
+                    </td>
+                    <td>
+                        ReviewTime
+                    </td>
+                    <td>
+                        Commit Person
+                    </td>
+                </thead>
+                <tr>
+                    <td>
+                        {{data.id}}
+                    </td>
+                    <td>
+                        {{data.reviewer}}
+                    </td>
+                    <td>
+                        {{data.reviewtime}}
+                    </td>
+                    <td>
+                        {{data.committer}}
+                    </td>
+                </tr>
+            </table>
+        </div>
     `,
 })
 
 export class AppComponent {
     title = 'The committed details of This Project';
+    data: ReviewData = {
+        id: "zkasdf1234",
+        reviewer: "00316496",
+        reviewtime: "201702242200",
+        committer: "00388224",
+    };
+}
+
+export class ReviewData {
+    id: string;
+    reviewer: string;
+    reviewtime: string;
+    committer: string;
 }
